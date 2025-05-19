a147fcaefa4c0f5b4de1db5e3552a9cc18a8ca41
Miguel Schneider <miguel@schneiderlein.ch>
2025-05-19 23:11:53 +0200

    update pytest for Flask service ground structure
    #12

--- ressources/licensetool/test/create_test_data.py
+++ ressources/licensetool/test/create_test_data.py
@@ -6,8 +6,8 @@ def create_test_data():
     db.create_all() # Create all tables
     
     # Create test data
-    license1 = LicenseModel(name='Test License 1', description='This is a test license 1')
-    license2 = LicenseModel(name='Test License 2', description='This is a test license 2')
+    license1 = LicenseModel(name='Test License 1', count=10)
+    license2 = LicenseModel(name='Test License 2', count=5)
     
     # Add test data to the session    
     db.session.add(license1)
--- ressources/licensetool/test/test_license.py
+++ ressources/licensetool/test/test_license.py
@@ -1,19 +1,18 @@
 from test import client
 
-def test_get_license(client):
+def test_get_licenses(client):
     """Test the get license endpoint."""
-    response = client.get('/license')
+    response = client.get('/api/v1/licenses/')
     assert response.status_code == 200
-    assert b'License' in response.data
+    assert b'Test License 1' in response.data
     
 def test_get_license_by_id(client):
     """Test the get license by id endpoint."""
-    response = client.get('/license/1')
+    response = client.get('/api/v1/licenses/1')
     assert response.status_code == 200
-    assert b'License' in response.data
+    assert b'Test License 1' in response.data
     
 def test_get_license_by_id_not_found(client):
     """Test the get license by id endpoint with a non-existing id."""
-    response = client.get('/license/99999999')
+    response = client.get('/api/v1/licenses/99999999')
     assert response.status_code == 404
-    assert b'License not found' in response.data
